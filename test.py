3e8d8f201dbf36f797227d85c818d69fe9e301ca
YA <YA-androidapp@users.noreply.github.com>
2019-05-23 08:55:27 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2019 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+
 # $ python -m venv watson
 # $ . watson/bin/activate
 # (watson) $ pip install --upgrade 'ibm-watson'
 
+from glob import glob
 from ibm_watson import VisualRecognitionV3
 import configparser
+import datetime
+import os
 import json
+
+
+scrpath = os.path.abspath(os.path.dirname(__file__))
+os.chdir(scrpath)
+
+# このスクリプトと同じディレクトリにtestフォルダを作成、
+# そのサブディレクトリに訓練データと検証データからなるデータセットを格納
+root_test_dirname = 'test'
+
+# テスト結果を出力するテキストファイル名
+nowstr = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+result_filename = 'result-test-'+nowstr+'.txt'
+
+# 正答率評価のための変数
+count_items_all = 0
+count_items_correct = 0
 
 # 設定ファイル読み込み
 inifile = configparser.ConfigParser()
@@ -29,17 +50,54 @@ def classify(path):
                 images_file,
                 threshold='0.83',
                 classifier_ids='DefaultCustomModel_1970291170').get_result()
-        # print(json.dumps(classes, indent=2))
-        label, score = classes['images'][0]['classifiers'][0]['classes'][0][
-            'class'], classes['images'][0]['classifiers'][0]['classes'][0]['score']
+        print(json.dumps(classes, indent=2))
+        # TODO: check
+        # {
+        #   "images": [
+        #     {
+        #       "classifiers": [
+        #         {
+        #           "classes": []
+        label, score = classes['images'][0]['classifiers'][0]['classes'][0]['class'], classes['images'][0]['classifiers'][0]['classes'][0]['score']
         print(label, score)
-
+        return label
     except Exception as e:
         print('[Err] {0}'.format(e))
+    return ''
 
 
 def main():
-    classify('test.png')
+    global count_items_all
+    global count_items_correct
+
+    # テスト用画像取得
+    subdirs = glob(os.path.join(
+        scrpath, root_test_dirname, '**'))
+    for subdir in subdirs:
+        if os.path.isdir(subdir):
+            print('sub directory: {}'.format(subdir), datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'))
+            testdatas = glob(os.path.join(subdir, '*.png'))
+            count_testfile = 0
+            answer = os.path.basename(subdir)
+            for testdata in testdatas:
+                print('  {} {:.2%} {}'.format(
+                    answer, (count_testfile/len(testdatas)), testdata), datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'))
+                label = classify(testdata)
+
+                count_testfile += 1
+                count_items_all += 1
+                if label == answer:
+                    count_items_correct += 1
+
+    if count_items_all > 0:
+        mes = 'Complete. accuracy:{} / {} = {:.2%}'.format(
+            count_items_correct, count_items_all, count_items_correct / count_items_all)
+    else:
+        mes = 'Complete.'
+    mes += ' ' + datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    print(mes)
+    with open(os.path.join(scrpath, result_filename), mode='a') as f:
+        f.write(mes + '\n')
 
 
 if __name__ == '__main__':
